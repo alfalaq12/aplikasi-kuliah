6764f01e1668887daa166f18b8ab0e42bd9b7104
alfalaq12 <alfalaqpurnomo@gmail.com>
2024-09-12 13:19:20 +0700

    membuat website aplikasi kuliah

---
+++ js/routes.js
@@ -0,0 +1,7 @@
+var routes = [
+    {path: '/', componentUrl: 'pages/home.html'},
+    {path: '/kantor/', componentUrl: 'pages/kantor.html'},
+    {path: '/karyawan/', componentUrl: 'pages/karyawan.html'},
+    {path: '(.*)', url: 'pages/404.html'}
+];
+
